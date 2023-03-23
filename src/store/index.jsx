9f509d22274ff2c061dacdbe626d9519e7e5f220
ayeolakenny <ayeolakenny@gmail.com>
2023-03-23 16:30:11 +0100

    feature:added wallet connect

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,7 +2,7 @@ import { createGlobalState } from "react-hooks-global-state";
 
 const { setGlobalState, useGlobalState, getGlobalState } = createGlobalState({
   connectedAccount: "",
-  isWalletModalOpen: false,
+  isWalletModalOpen: true,
 });
 
 export { useGlobalState, setGlobalState, getGlobalState };
