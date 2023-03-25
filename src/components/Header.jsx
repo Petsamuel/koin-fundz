cfd1787e375c5bc5fc449f155df1d9a992e8a155
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-03-25 18:02:22 +0100

    chore: revarted header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,14 +13,14 @@ const ConnectWalletButton = () => {
 
 export const Header = () => {
   return (
-    <Disclosure as="nav" className="bg-white w-screen fixed z-10">
+    <Disclosure as="nav" className="bg-gray-800 fixed z-10 w-screen">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between ">
-              <div className="lg:absolute inset-y-0 left-0 flex items-center sm:hidden justify-between w-screen sm:flex-row-reverse">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -29,107 +29,90 @@ export const Header = () => {
                   )}
                 </Disclosure.Button>
               </div>
-              {/* 1 */}
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center sm:mx-4">
+                <div className="flex flex-shrink-0 items-center">
                   <img
                     className="block h-8 w-auto lg:hidden"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                     alt="Your Company"
                   />
-                  <div>
-                    <img
-                      className="hidden h-8 w-auto lg:block"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                      alt="Your Company"
-                    />
-                  </div>
+                  <img
+                    className="hidden h-8 w-auto lg:block"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
                 </div>
-              </div>
-              {/* end 1 */}
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="hidden sm:ml-6 sm:flex">
+                <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     <a
                       href="#"
-                      className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                     >
                       Home
                     </a>
                     <a
                       href="#"
-                      className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                     >
-                      About
+                      About Us
                     </a>
                     <a
                       href="#"
-                      className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                     >
-                      Service
+                      Services
                     </a>
                     <a
                       href="#"
-                      className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                     >
                       FAQ
                     </a>
                   </div>
-                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <Menu as="div" className="relative ml-3">
-                      <div className="flex justify-center">
-                        <Menu.Button>
-                          <ConnectWalletButton />
-                        </Menu.Button>
-                      </div>
-                    </Menu>
-                  </div>
                 </div>
-
               </div>
-
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <Menu as="div" className="relative ml-3">
+                  <div className="flex justify-center">
+                    <Menu.Button>
+                      <ConnectWalletButton />
+                    </Menu.Button>
+                  </div>
+                </Menu>
+              </div>
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden block">
+          <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="text-gray-500 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               >
                 Home
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="text-gray-500 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               >
-                About
+                About Us
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="text-gray-500 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               >
-                Service
+                Services
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="text-gray-500 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
               >
-                FAQ
+                Faq
               </Disclosure.Button>
-            </div>
-            <div className="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 py-3">
-              <Menu as="div" className="relative ml-3">
-                <div className="flex justify-center">
-                  <Menu.Button>
-                    <ConnectWalletButton />
-                  </Menu.Button>
-                </div>
-              </Menu>
             </div>
           </Disclosure.Panel>
         </>
