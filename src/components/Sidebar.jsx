ca897dd0d58063673e92bf201d28724b03211da5
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-04-04 17:53:43 +0100

    chore: link object to fields

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ function Sidebar() {
         class={
           mobileToggle
             ? "top-0 left-0 z-20 w-64 h-screen transition-transform translate-x-0 sm:translate-x-0 py-8 absolute shadow-md bg-white"
-            : "hidden  lg:contents w-64 h-screen lg:absolute top-0 left-0"
+            : "sm:hidden top-0 left-0 z-20 w-64 h-screen transition-transform translate-x-0 sm:translate-x-0 py-8 absolute shadow-md bg-white"
         }
       >
         <div class="h-screen px-3 py-14">
