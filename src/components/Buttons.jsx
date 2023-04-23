0bd3107be5698c1f674fade8d9d6b4034c3d72dd
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2023-04-23 11:30:40 +0100

    fix: view project page

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { Web3Button } from "@web3modal/react";
+import { useGlobalState } from "../store";
+
+export const ConnectWalletButton = () => {
+  const [connectedAccount] = useGlobalState("connectedAccount");
+  useEffect(() => {}, [connectedAccount]);
+  return <Web3Button />;
+};
+
+export const ToggleButton = ({ handleEvent }) => {
+  return (
+    <div
+      type="button"
+      class="sm:inline-flex hidden items-center  p-2  ml-3 text-sm text-gray-500 rounded-lg  focus:outline-none focus:ring-2  dark:text-gray-400 z-30 relative"
+      onClick={handleEvent}
+    >
+      <span class="sr-only">Toggle Button</span>
+      <svg
+        class="w-4 h-4 border-none focus:border-none"
+        aria-hidden="true"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          clipRule="evenodd"
+          fill-rule="evenodd"
+          d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export const GenButton = ({ name, href }) => {
+  return (
+    <a
+      href="#"
+      class="mt-8 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+    >
+      {name}
+    </a>
+  );
+};
--- src/pages/ViewProject.jsx
+++ src/pages/ViewProject.jsx
@@ -1,7 +1,65 @@
+import { useParams } from "react-router-dom";
+import { useGlobalState } from "../store";
+import {GenButton} from "../components/Buttons"
+
 function ViewProject() {
-  return <>
-  
-  </>;
+  const [projectData, setProjectData] = useGlobalState("projects");
+  const { id } = useParams();
+
+  const data = projectData.filter((value, index) => id.includes(index));
+  // console.log(id);
+  console.log(data);
+
+  return (
+    <>
+      <div className="relative p-4 sm:mx-8 text-transparent py-8 ">
+        <div className="p-4  border-gray-200 rounded-lg  ">
+          <div className=" flex items-center  sm:min-w-[380px]  my-2 border-b-2  ">
+            <div className="pb-4 text-lg text-blueoff">View Project</div>
+          </div>
+          <section>
+            {data ? (
+              <>
+                {data.map((index) => (
+                  <>
+                    <div class="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
+                      <div class="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
+                        <div class="relative z-10 lg:py-16">
+                          <div class="relative h-64 sm:h-80 lg:h-full">
+                            <img src={index.imageUrl} alt="" srcset="" />
+                          </div>
+                        </div>
+
+                        <div class="relative flex items-center bg-gray-100">
+                          <span class="hidden lg:absolute lg:inset-y-0 lg:-start-16 lg:block lg:w-16 lg:bg-gray-100">
+                            {index.title}
+                          </span>
+
+                          <div class="p-8 sm:p-16 lg:p-24">
+                            <h2 class="text-2xl font-bold sm:text-3xl">
+                              {index.title}
+                            </h2>
+
+                            <p class="mt-4 text-gray-600">
+                              {index.description}
+                            </p>
+
+                            <GenButton name="Donate" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ))}
+              </>
+            ) : (
+              ""
+            )}
+          </section>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ViewProject;
